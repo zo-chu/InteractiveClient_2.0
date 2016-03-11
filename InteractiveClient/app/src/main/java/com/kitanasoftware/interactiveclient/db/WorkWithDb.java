38d1fe0a6e4f59e9208ac3a956ec4d25a884d254
dasha <dashapaster@gmail.com>
2016-03-11 17:42:14 +0200

    add notification table

--- InteractiveClient/app/src/main/java/com/kitanasoftware/interactiveclient/db/WorkWithDb.java
+++ InteractiveClient/app/src/main/java/com/kitanasoftware/interactiveclient/db/WorkWithDb.java
@@ -12,6 +12,8 @@ import com.kitanasoftware.interactiveclient.information.GuideInform;
 import com.kitanasoftware.interactiveclient.information.Information;
 import com.kitanasoftware.interactiveclient.information.TourInform;
 import com.kitanasoftware.interactiveclient.map.Geopoint;
+import com.kitanasoftware.interactiveclient.map.GeopointsData;
+import com.kitanasoftware.interactiveclient.notification.MyNotification;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -48,7 +50,30 @@ public class WorkWithDb {
     private JSONArray jsonArrayGeo;
 
     private JSONArray jsonArraySchedule;
+    private ArrayList<MyNotification> notificationList;
+    public ArrayList<MyNotification> getNotificationList() {
+        if(notificationList.size() == 0 ){
+            getNotifications();
+        }
+        return notificationList;
+    }
+    private ArrayList<MyNotification> getNotifications(){
+        String sentTo;
+        String text;
+        cursor = db.rawQuery("SELECT * FROM notifications", null);
+        int size = cursor.getCount();
+        if (size > 0){
+            cursor.moveToFirst();
+            for (int i = 0; i < size; i++) {
+                sentTo = cursor.getString(0);
+                text = cursor.getString(1);
+                notificationList.add(new MyNotification(sentTo, text));
+                cursor.moveToNext();
+            }
+        }
 
+        return notificationList;
+    }
 
     public ArrayList<Information> getInformList() {
         if(informList.size() == 0 ){
@@ -92,6 +117,7 @@ public class WorkWithDb {
     public static WorkWithDb getWorkWithDb(Context context){
         if(workWithDb==null){
             workWithDb = new WorkWithDb(context);
+
         }
         return workWithDb;
     }
@@ -206,7 +232,7 @@ public class WorkWithDb {
     public void updateGeopointByIndex(int index, String name, String type, int color, double[] coordinates){
         Geopoint geopoint = geopointList.get(index);
         db.execSQL("UPDATE geopoints set name='" + name + "',type='" + type+"',color= " +
-                color + "lan=" + coordinates[0] +", lon=" + coordinates[1] +" WHERE id=" + index + "");
+                color + ",lat=" + coordinates[0] +", lon=" + coordinates[1] +" WHERE id=" + index + "");
 
         geopoint.setName(name);
         geopoint.setType(type);
@@ -253,6 +279,12 @@ public class WorkWithDb {
     public void addInformation(String guideName, String guidePhone, String tour, String goal, String company ){
         int inf_id = getInformList().size();
         db.execSQL("INSERT INTO information VALUES ('" + guideName + "', '" + guidePhone + "', '" + tour + "','" + goal + "','" + company + "')");
+
+    }
+    public void addNotification(String sentTo, String text){
+        int index =getNotificationList().size();
+        getScheduleList().add(new Schedule(sentTo, text));
+        db.execSQL("INSERT INTO notifications VALUES (" + index + ", '" + sentTo + "', '" + text + "')");
 
     }
     public void addIp(String ip ){
